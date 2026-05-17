8b45c0d527640d18692d8c7412f69fe210a2bd19
Miere Liniel Teixeira <miere00@gmail.com>
2026-05-17 13:39:09 +1000

    Add Auggie support

---
+++ litellm_acp_router/adapters/auggie.py
@@ -0,0 +1,14 @@
+from .static import StaticAdapter
+
+
+class AuggieAdapter(StaticAdapter):
+    def __init__(self) -> None:
+        super().__init__(
+            agent_id="auggie",
+            default_bin="auggie",
+            default_args=["--acp"],
+            default_mode_id=None,
+            aliases=["augment", "augment-code"],
+            env_var_prefix="AUGGIE",
+            acp_model_arg="--model",
+        )
